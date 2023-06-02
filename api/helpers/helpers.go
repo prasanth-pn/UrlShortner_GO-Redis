40db7aeb5fd9acf201b22c5056c94c56e00dc62b
PRASANTH PN <118249131+prasanth-pn@users.noreply.github.com>
2023-06-02 18:20:00 +0530

    database connected

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -17,7 +17,13 @@ func RemoveDomainError(url string) bool {
 		return false
 	}
 	newURL := strings.Replace(url, "http://", "", 1)
-	newURL=strings.Replace(newURL,"https://","",1)
-	
+	newURL = strings.Replace(newURL, "https://", "", 1)
+	newURL = strings.Replace(newURL, "www.", "", 1)
+	newURL = strings.Split(newURL, "/")[0]
+
+	if newURL == os.Getenv("DOMAIN") {
+		return false
+	}
+
 	return true
 }
--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,5 +1,24 @@
 package routes
 
-func ResolveURL(){
-	
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/prasanth-pn/UrlShortner_GO-Redis/database"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error:": "short not found"})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to db",
+		})
+	}
+	rIr := database.CreateClient(1)
+	defer rInr.Close()
 }
